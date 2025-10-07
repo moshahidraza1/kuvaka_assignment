a4098ccf16c5b791ff298a32ee68bf7b481c768e
moshahidraza1 <codinghandle@gmail.com>
2025-10-07 15:12:08 +0530

    added enpoint for leads upload

--- src/routes/inputRoutes.js
+++ src/routes/inputRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
 import { setOfferDetails, uploadLeads } from "../controllers/inputController.js";
 
 const router = Router();
 
 // offer details endpoint
 router.post('/offerDetail',setOfferDetails);
+
+// lead uploads
+const upload = multer();
+
+router.post('/uploadLeads', upload.single('file'), uploadLeads);
 
 export default router;
