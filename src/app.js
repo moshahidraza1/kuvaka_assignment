4e0d7da6293bdda59e42b7e1346365004e5ef178
moshahidraza1 <codinghandle@gmail.com>
2025-10-07 15:17:10 +0530

    initialized inpur routes in app.js file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
 
+import inputRoutes from './routes/inputRoutes.js';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/input', inputRoutes);
 
 export {app};
